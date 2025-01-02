ff055f3727527856c6b82759dc4f8401470186f8
wuchuanming <wuchuanming1990@gmail.com>
2025-01-02 13:28:08 +0800

    基础功能拆分到单独的user control中

--- Calculator/ViewModels/MainWindowViewModel.cs
+++ Calculator/ViewModels/MainWindowViewModel.cs
@@ -1,87 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 
 namespace Calculator.ViewModels;
 
 public partial class MainWindowViewModel : ObservableObject
 {
-    [ObservableProperty] private string _display = "0";
+    public BasicCalculatorViewModel Calculator { get; }
 
-    private double _lastNumber = 0;
-    private string _currentOperator = "";
-    private bool _newNumber = true;
-
-    [RelayCommand]
-    private void Number(string number)
+    public MainWindowViewModel()
     {
-        if (_newNumber)
-        {
-            Display = number;
-            _newNumber = false;
-        }
-        else
-        {
-            Display = Display == "0" ? number : Display + number;
-        }
-    }
-
-    [RelayCommand]
-    private void Del()
-    {
-        if (Display.Length == 1)
-        {
-            Display = "0";
-        }
-        else
-        {
-            Display = Display.Remove(Display.Length - 1);
-        }
-    }
-
-    [RelayCommand]
-    private void Dot()
-    {
-        if (!_newNumber && !Display.Contains("."))
-        {
-            Display += ".";
-        }
-    }
-
-    [RelayCommand]
-    private void Operator(string op)
-    {
-        _lastNumber = double.Parse(Display);
-        _currentOperator = op;
-        _newNumber = true;
-    }
-
-    [RelayCommand]
-    private void Clear()
-    {
-        Display = "0";
-        _lastNumber = 0;
-        _currentOperator = "";
-        _newNumber = true;
-    }
-
-    [RelayCommand]
-    private void Equal()
-    {
-        if (string.IsNullOrEmpty(_currentOperator)) return;
-
-        double currentNumber = double.Parse(Display);
-        double result = _currentOperator switch
-        {
-            "+" => _lastNumber + currentNumber,
-            "-" => _lastNumber - currentNumber,
-            "X" => _lastNumber * currentNumber,
-            "÷" => _lastNumber / currentNumber,
-            "%" => _lastNumber % currentNumber,
-            _ => currentNumber
-        };
-
-        Display = result.ToString();
-        _currentOperator = "";
-        _newNumber = true;
+        Calculator = new BasicCalculatorViewModel();
     }
 }
---
+++ Calculator/Views/BasicCalculatorView.axaml.cs
@@ -0,0 +1,12 @@
+
+using Avalonia.Controls;
+
+namespace Calculator.Views;
+
+public partial class BasicCalculatorView : UserControl
+{
+    public BasicCalculatorView()
+    {
+        InitializeComponent();
+    }
+}
